e8ad06f4be5aac99a116586c14c162be889d7714
anil <Anil-Gehlot>
2024-03-19 00:05:53 +0530

    new change 1.1

--- Database_Generator/__init__.py
+++ Database_Generator/__init__.py
@@ -1,23 +1,15 @@
 
 from flask import Flask, render_template, request, redirect, url_for, send_file
 import requests
-import utils.api
+# import utils.api
 
 
 app = Flask(__name__)
 
 
-
-
-
-
-
-
-
-
 @app.route('/')
 def index():
-    print()
+    
     return render_template('index.html')
 
 
