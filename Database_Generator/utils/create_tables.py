94d65c95add2480018a57d2be19a5fc1cfdbce54
Ps1231 <sonipriyanshi.ps99@gmail.com>
2024-03-20 03:40:07 +0530

    timeseries

--- Database_Generator/utils/create_tables.py
+++ Database_Generator/utils/create_tables.py
@@ -15,41 +15,36 @@ db_config = {
 }
 
 
-#---------------------------------------------------
+# ---------------------------------------------------
 
-#  function for creating a table
+# ---------------------------------------------------
 def create_table(column_detail, table_name):
 
-    # splitting column_detail by ","   
+    # splitting column_detail by ","
     column_list = column_detail.split(",")
-    
+
     # list to store the column details after splitting by ":"
     column_detail_list = []
-    
+
     # splitting each column by ":" and storing in column_detail_list
     for column in column_list:
         s = column.split(":")
         column_detail_list.append(s)
+    column_detail_list.append(['timestamp', 'timestamp'])
 
-    # Adding two additional columns for timestamp and temperature
-    # column_detail_list.append(['timestamp', 'timestamp'])
-        
-    # MySQL create table query 
-    query = f"create table {table_name} ("
-       
-    # iterating through the list of column details 
+    # MySQL create table query
+    query = f"CREATE TABLE {table_name} ("
+
+    # iterating through the list of column details
     for column_info in column_detail_list:
-
-        # iterating through the inner list
+        # Iterating through the inner list
         for detail in column_info:
             query += f" {detail}"
         if column_info != column_detail_list[-1]:
-            query += " , "
-    
+            query += ","
+
     # ending part of query
     query += ");"
 
     # returning the MySQL create table query
     return query
-
-#-----------------------------------------------------
--- Database_Generator/utils/insert_query.py
+++ Database_Generator/utils/insert_query.py
@@ -12,7 +12,7 @@ openai_api_key = config["openai_api_key"]
 def generate_insert_query(table_name, col_details):
     headers = {"Authorization": f"Bearer {openai_api_key}"}
 
-    instruction = f"write insert query with real life data that doesn't seem generated and looks indian 10 entries for table {table_name} having columns: {col_details} Also make sure that size of each entry for all columns should be strictly according to the size given in round bracket after datatype"
+    instruction = f"write insert query with real life data that doesn't seem generated and looks indian 10 entries for table {table_name} having columns: {col_details} also there is additional column timestamp datatype TIMESTAMP and in that column add current time in format '%Y-%m-%d %H:%M:%S' only and don't generate NOW(). Also make sure that size of each entry for all columns should be strictly according to the size given in round bracket after datatype  "
 
     url = "https://api.edenai.run/v2/text/code_generation"
     payload = {
@@ -20,15 +20,13 @@ def generate_insert_query(table_name, col_details):
         "prompt": "",
         "instruction": instruction,
         "temperature": 0.1,
-        "max_tokens": 500,
+        "max_tokens": 2000,
         "fallback_providers": ""
     }
 
     response = requests.post(url, json=payload, headers=headers)
 
     result = json.loads(response.text)
-
-    print(result)
 
     generated_text = result['openai']['generated_text']
 
@@ -45,11 +43,7 @@ def generate_insert_query(table_name, col_details):
     sql_query = sql_query[:end_index]
 
     # Check if the character before "```" is a semicolon, if not, add a semicolon
-    if not re.search(r';```', sql_query):
-        semicolon_index = sql_query.rfind(';')
-        if semicolon_index == -1:
-            semicolon_index = 0
-        sql_query = sql_query[:semicolon_index] + \
-            ';' + sql_query[semicolon_index:]
+    if not sql_query.endswith(";"):
+        sql_query += ";"
 
     return sql_query
